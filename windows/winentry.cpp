6e4d3160488d7e6f987e9a63fec7645c1e5d836f
András Bögöly <andras.bogoly.91@gmail.com>
2021-01-17 17:48:29 +0100

    Display a circle with Direct2D

--- windows/winentry.cpp
+++ windows/winentry.cpp
@@ -29,90 +29,56 @@ processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 #include <d2d1.h>
 #pragma comment(lib, "d2d1")
 
-template <class DERIVED_TYPE>
-class BaseWindow
+#include "basewin.h"
+
+template <class T> void SafeRelease(T** ppT)
 {
-public:
-    static LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+    if (*ppT)
     {
-        DERIVED_TYPE* pThis = NULL;
-
-        if (uMsg == WM_NCCREATE)
-        {
-            CREATESTRUCT* pCreate = reinterpret_cast<CREATESTRUCT*>(lParam);
-            pThis = reinterpret_cast<DERIVED_TYPE*>(pCreate->lpCreateParams);
-            SetWindowLongPtr(hwnd, GWLP_USERDATA, (LONG_PTR)pThis);
-            pThis->m_hwnd = hwnd;
-        }
-        else
-        {
-            pThis = reinterpret_cast<DERIVED_TYPE*>(GetWindowLongPtr(hwnd, GWLP_USERDATA));
-        }
-
-        if (pThis)
-        {
-            return pThis->HandleMessage(uMsg, wParam, lParam);
-        }
-        else
-        {
-            return DefWindowProc(hwnd, uMsg, wParam, lParam);
-        }
+        (*ppT)->Release();
+        *ppT = NULL;
     }
-
-    BaseWindow() : m_hwnd(NULL) { }
-
-    BOOL Create(
-        PCWSTR lpWindowName,
-        DWORD dwStyle,
-        DWORD dwExStyle = 0,
-        int x = CW_USEDEFAULT,
-        int y = CW_USEDEFAULT,
-        int nWidth = CW_USEDEFAULT,
-        int nHeight = CW_USEDEFAULT,
-        HWND hWndParent = 0,
-        HMENU hMenu = 0
-    )
-    {
-        WNDCLASS wc = { 0 };
-
-        wc.lpfnWndProc = DERIVED_TYPE::WindowProc;
-        wc.hInstance = GetModuleHandle(NULL);
-        wc.lpszClassName = ClassName();
-
-        RegisterClass(&wc);
-
-        m_hwnd = CreateWindowEx(
-            dwExStyle, ClassName(), lpWindowName, dwStyle, x, y,
-            nWidth, nHeight, hWndParent, hMenu, GetModuleHandle(NULL), this
-        );
-
-        return (m_hwnd ? TRUE : FALSE);
-    }
-
-    HWND Window() const { return m_hwnd; }
-
-protected:
-
-    virtual PCWSTR  ClassName() const = 0;
-    virtual LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam) = 0;
-
-    HWND m_hwnd;
-};
-
+}
 
 class MainWindow : public BaseWindow<MainWindow>
 {
+
+private:
+
+    ID2D1Factory*          pFactory;
+    ID2D1HwndRenderTarget* pRenderTarget;
+    ID2D1SolidColorBrush*  pBrush;
+    D2D1_ELLIPSE           ellipse;
+
+    int32_t times = 0;
+
+    void    CalculateLayout();
+    HRESULT CreateGraphicsResources();
+    void    DiscardGraphicsResources();
+    void    OnPaint();
+    void    Resize();
+
 public:
+    MainWindow() : pFactory(NULL), pRenderTarget(NULL), pBrush(NULL), ellipse({}) { }
+
     PCWSTR  ClassName() const { return L"TicTacToe Window Class"; }
     LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam);
-private:
-    int32_t times = 0;
 };
 
 LRESULT MainWindow::HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
     switch (uMsg)
     {
+    case WM_CREATE:
+        {
+            HRESULT result = D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, &pFactory);
+            if (FAILED(result))
+            {
+                // Fail CreateWindowEx.
+                return -1;
+            }
+        }
+        return 0;
     case WM_CLOSE:
         {
             std::wstring s(L"Really quit? ");
@@ -125,17 +91,16 @@ LRESULT MainWindow::HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
         }
         return 0;
     case WM_DESTROY:
+        DiscardGraphicsResources();
+        SafeRelease(&pFactory);
         PostQuitMessage(0);
         return 0;
     case WM_PAINT:
-        {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(m_hwnd, &ps);
-            FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
-            EndPaint(m_hwnd, &ps);
-        }
-    return 0;
-
+        OnPaint();
+        return 0;
+    case WM_SIZE:
+        Resize();
+        return 0;
     default:
         return DefWindowProc(m_hwnd, uMsg, wParam, lParam);
     }
@@ -144,14 +109,14 @@ LRESULT MainWindow::HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
 
 int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_  PWSTR pCmdLine, _In_  int nCmdShow)
 {
-    MainWindow win;
+    MainWindow mainWindow;
 
-    if (!win.Create(L"TicTacToe", WS_OVERLAPPEDWINDOW))
+    if (!mainWindow.Create(L"TicTacToe", WS_OVERLAPPEDWINDOW))
     {
         return 0;
     }
 
-    ShowWindow(win.Window(), nCmdShow);
+    ShowWindow(mainWindow.Window(), nCmdShow);
 
     // Run the message loop.
 
@@ -163,4 +128,99 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
     }
 
     return 0;
+}
+
+void MainWindow::CalculateLayout()
+{
+    if (pRenderTarget == NULL) {
+        return;
+    }
+    D2D1_SIZE_F size = pRenderTarget->GetSize();
+    const float x = size.width / 2;
+    const float y = size.height / 2;
+    const float radius = min(x, y);
+    ellipse = D2D1::Ellipse(D2D1::Point2F(x, y), radius, radius);
+}
+
+HRESULT MainWindow::CreateGraphicsResources()
+{
+    if (pRenderTarget != NULL)
+    {
+        //Don't initialize twice
+        return S_OK;
+    }
+
+    RECT rc;
+    GetClientRect(m_hwnd, &rc);
+
+    D2D1_SIZE_U size = D2D1::SizeU(rc.right, rc.bottom);
+
+    HRESULT hr = pFactory->CreateHwndRenderTarget(
+        D2D1::RenderTargetProperties(),
+        D2D1::HwndRenderTargetProperties(m_hwnd, size),
+        &pRenderTarget);
+
+    if (!SUCCEEDED(hr))
+    {
+        return hr;
+    }
+
+    const D2D1_COLOR_F color = D2D1::ColorF(1.0f, 1.0f, 0);
+    hr = pRenderTarget->CreateSolidColorBrush(color, &pBrush);
+
+    if (!SUCCEEDED(hr))
+    {
+        return hr;
+    }
+    
+    CalculateLayout();
+    return hr;
+
+}
+
+void MainWindow::DiscardGraphicsResources()
+{
+    SafeRelease(&pRenderTarget);
+    SafeRelease(&pBrush);
+}
+
+void MainWindow::OnPaint()
+{
+    HRESULT hr = CreateGraphicsResources();
+    if (!SUCCEEDED(hr))
+    {
+        return;
+    }
+
+    PAINTSTRUCT ps;
+    BeginPaint(m_hwnd, &ps);
+
+    pRenderTarget->BeginDraw();
+
+    pRenderTarget->Clear(D2D1::ColorF(D2D1::ColorF::SkyBlue));
+    pRenderTarget->FillEllipse(ellipse, pBrush);
+
+    hr = pRenderTarget->EndDraw();
+    if (FAILED(hr) || hr == D2DERR_RECREATE_TARGET)
+    {
+        DiscardGraphicsResources();
+    }
+    EndPaint(m_hwnd, &ps);
+    
+}
+
+void MainWindow::Resize()
+{
+    if (pRenderTarget == NULL)
+    {
+        return;
+    }
+    RECT rc;
+    GetClientRect(m_hwnd, &rc);
+
+    D2D1_SIZE_U size = D2D1::SizeU(rc.right, rc.bottom);
+
+    pRenderTarget->Resize(size);
+    CalculateLayout();
+    InvalidateRect(m_hwnd, NULL, FALSE);
 }
