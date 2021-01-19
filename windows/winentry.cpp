d2026c913641e4dab23d92436c38d051aa90f9cb
András Bögöly <andras.bogoly.91@gmail.com>
2021-01-19 22:41:40 +0100

    Use C++ style resource management instead

--- windows/winentry.cpp
+++ windows/winentry.cpp
@@ -31,39 +31,82 @@ processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 
 #include "basewin.h"
 
-template <class T> void SafeRelease(T** ppT)
+class MainWindowLogic
 {
-    if (*ppT)
-    {
-        (*ppT)->Release();
-        *ppT = NULL;
-    }
-}
-
-class MainWindow : public BaseWindow<MainWindow>
-{
-
 private:
 
-    ID2D1Factory*          pFactory;
-    ID2D1HwndRenderTarget* pRenderTarget;
-    ID2D1SolidColorBrush*  pBrush;
-    D2D1_ELLIPSE           ellipse;
+    HWND m_hwnd;
+
+    std::unique_ptr<ID2D1Factory> pFactory;
+    std::unique_ptr<ID2D1HwndRenderTarget> pRenderTarget;
+    std::unique_ptr<ID2D1SolidColorBrush> pBrush;
+    D2D1_ELLIPSE ellipse;
 
     int32_t times = 0;
 
-    void    CalculateLayout();
-    HRESULT CreateGraphicsResources();
-    void    DiscardGraphicsResources();
-    void    OnPaint();
-    void    Resize();
-
 public:
-    MainWindow() : pFactory(NULL), pRenderTarget(NULL), pBrush(NULL), ellipse({}) { }
+    MainWindowLogic(HWND m_hwnd);
+
+    void    CalculateLayout();
+    void    OnPaint();
+    void    OnResize();
+    void    OnClose();
+
+};
+
+class MainWindow : public BaseWindow<MainWindow>
+{
+private:
+    std::unique_ptr<MainWindowLogic> mainWindowSub;
+public:
+    MainWindow() { }
 
     PCWSTR  ClassName() const { return L"TicTacToe Window Class"; }
     LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam);
 };
+
+MainWindowLogic::MainWindowLogic(HWND m_hwnd) :
+    m_hwnd{ m_hwnd },
+    ellipse{ {} }
+{
+    //Initialize Direct2D factory
+    ID2D1Factory* pFactoryResult = NULL;
+    HRESULT result = D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, &pFactoryResult);
+    if (FAILED(result))
+    {
+        throw - 1;
+    }
+    pFactory.reset(pFactoryResult);
+
+    //Get size of window
+    RECT rc;
+    GetClientRect(m_hwnd, &rc);
+    D2D1_SIZE_U size = D2D1::SizeU(rc.right, rc.bottom);
+
+    //Create stuff
+    ID2D1HwndRenderTarget* pRenderTargetResult = NULL;
+    result = pFactory->CreateHwndRenderTarget(
+        D2D1::RenderTargetProperties(),
+        D2D1::HwndRenderTargetProperties(m_hwnd, size),
+        &pRenderTargetResult);
+
+    if (FAILED(result))
+    {
+        throw - 1;
+    }
+    pRenderTarget.reset(pRenderTargetResult);
+
+    ID2D1SolidColorBrush* pBrushResult = NULL;
+    const D2D1_COLOR_F color = D2D1::ColorF(1.0f, 1.0f, 0);
+    result = pRenderTarget->CreateSolidColorBrush(color, &pBrushResult);
+    if (FAILED(result))
+    {
+        throw - 1;
+    }
+    pBrush.reset(pBrushResult);
+
+    CalculateLayout();
+}
 
 LRESULT MainWindow::HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
@@ -71,35 +114,26 @@ LRESULT MainWindow::HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
     {
     case WM_CREATE:
         {
-            HRESULT result = D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, &pFactory);
-            if (FAILED(result))
-            {
-                // Fail CreateWindowEx.
+            try {
+                mainWindowSub.reset(new MainWindowLogic(m_hwnd));
+            }
+            catch (const std::exception& e) {
                 return -1;
             }
         }
         return 0;
     case WM_CLOSE:
-        {
-            std::wstring s(L"Really quit? ");
-            s += std::to_wstring(times++);
-            if (MessageBox(m_hwnd, s.c_str(), L"Confirmation on exit", MB_OKCANCEL) == IDOK)
-            {
-                DestroyWindow(m_hwnd);
-            }
-            // Else: User canceled. Do nothing.
-        }
+        mainWindowSub->OnClose();
         return 0;
     case WM_DESTROY:
-        DiscardGraphicsResources();
-        SafeRelease(&pFactory);
+        mainWindowSub.release();
         PostQuitMessage(0);
         return 0;
     case WM_PAINT:
-        OnPaint();
+        mainWindowSub->OnPaint();
         return 0;
     case WM_SIZE:
-        Resize();
+        mainWindowSub->OnResize();
         return 0;
     default:
         return DefWindowProc(m_hwnd, uMsg, wParam, lParam);
@@ -130,7 +164,9 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
     return 0;
 }
 
-void MainWindow::CalculateLayout()
+
+
+void MainWindowLogic::CalculateLayout()
 {
     if (pRenderTarget == NULL) {
         return;
@@ -142,79 +178,27 @@ void MainWindow::CalculateLayout()
     ellipse = D2D1::Ellipse(D2D1::Point2F(x, y), radius, radius);
 }
 
-HRESULT MainWindow::CreateGraphicsResources()
+void MainWindowLogic::OnPaint()
 {
-    if (pRenderTarget != NULL)
-    {
-        //Don't initialize twice
-        return S_OK;
-    }
-
-    RECT rc;
-    GetClientRect(m_hwnd, &rc);
-
-    D2D1_SIZE_U size = D2D1::SizeU(rc.right, rc.bottom);
-
-    HRESULT hr = pFactory->CreateHwndRenderTarget(
-        D2D1::RenderTargetProperties(),
-        D2D1::HwndRenderTargetProperties(m_hwnd, size),
-        &pRenderTarget);
-
-    if (!SUCCEEDED(hr))
-    {
-        return hr;
-    }
-
-    const D2D1_COLOR_F color = D2D1::ColorF(1.0f, 1.0f, 0);
-    hr = pRenderTarget->CreateSolidColorBrush(color, &pBrush);
-
-    if (!SUCCEEDED(hr))
-    {
-        return hr;
-    }
-    
-    CalculateLayout();
-    return hr;
-
-}
-
-void MainWindow::DiscardGraphicsResources()
-{
-    SafeRelease(&pRenderTarget);
-    SafeRelease(&pBrush);
-}
-
-void MainWindow::OnPaint()
-{
-    HRESULT hr = CreateGraphicsResources();
-    if (!SUCCEEDED(hr))
-    {
-        return;
-    }
-
     PAINTSTRUCT ps;
     BeginPaint(m_hwnd, &ps);
 
     pRenderTarget->BeginDraw();
 
     pRenderTarget->Clear(D2D1::ColorF(D2D1::ColorF::SkyBlue));
-    pRenderTarget->FillEllipse(ellipse, pBrush);
+    pRenderTarget->FillEllipse(ellipse, pBrush.get());
 
-    hr = pRenderTarget->EndDraw();
+    HRESULT hr = pRenderTarget->EndDraw();
     if (FAILED(hr) || hr == D2DERR_RECREATE_TARGET)
     {
-        DiscardGraphicsResources();
+        throw - 1;
     }
     EndPaint(m_hwnd, &ps);
     
 }
 
-void MainWindow::Resize()
+void MainWindowLogic::OnResize()
 {
-    if (pRenderTarget == NULL)
-    {
-        return;
-    }
     RECT rc;
     GetClientRect(m_hwnd, &rc);
 
@@ -223,4 +207,15 @@ void MainWindow::Resize()
     pRenderTarget->Resize(size);
     CalculateLayout();
     InvalidateRect(m_hwnd, NULL, FALSE);
+}
+
+void MainWindowLogic::OnClose()
+{
+    std::wstring s(L"Really quit? ");
+    s += std::to_wstring(times++);
+    if (MessageBox(m_hwnd, s.c_str(), L"Confirmation on exit", MB_OKCANCEL) == IDOK)
+    {
+        DestroyWindow(m_hwnd);
+    }
+    // Else: User canceled. Do nothing.
 }
