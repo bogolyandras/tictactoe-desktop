a7b4a7e8de139a834fe89a23de519b236cb33c85
András Bögöly <andras.bogoly.91@gmail.com>
2022-08-11 21:01:54 +0200

    GTK4 hello world!

--- gtk4/src/gtkmain.cxx
+++ gtk4/src/gtkmain.cxx
@@ -13,21 +13,16 @@ activate (GtkApplication *app,
 {
   GtkWidget *window;
   GtkWidget *button;
-  GtkWidget *button_box;
 
   window = gtk_application_window_new (app);
   gtk_window_set_title (GTK_WINDOW (window), "Window");
   gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
 
-  button_box = gtk_button_box_new (GTK_ORIENTATION_HORIZONTAL);
-  gtk_container_add (GTK_CONTAINER (window), button_box);
-
   button = gtk_button_new_with_label ("Hello World");
   g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
-  g_signal_connect_swapped (button, "clicked", G_CALLBACK (gtk_widget_destroy), window);
-  gtk_container_add (GTK_CONTAINER (button_box), button);
+  gtk_window_set_child (GTK_WINDOW (window), button);
 
-  gtk_widget_show_all (window);
+  gtk_window_present (GTK_WINDOW (window));
 }
 
 int
